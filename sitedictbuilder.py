5f4bc0353771f01989d5a2b8c619907737dfcfb5
bdauer <dauer.benjamin@gmail.com>
2017-03-06 11:50:12 -0500

    Add create_table call to the lambda

--- sitedictbuilder.py
+++ sitedictbuilder.py
@@ -1,10 +1,24 @@
 from __future__ import print_function
 import boto3
-# import requests
-
-
+import json
+import io
+from tablebuilder import create_table
 
 def lambda_handler(event, context):
+    '''
+    create a siteDict table.
+    use one of the values as the primarykey for each item. e.g. rank.
+    '''
+    # table is created here so that it's only created once.
+    table_name = 'siteDict'
+    attribute_list = [('name', 'S', 'HASH'), ('rank', 'N', 'RANGE')]
+    create_table(table_name, attribute_list)
+
+    lambda_client = boto3.client('lambda', region_name='us-east-1')
 
     for site in event['sites']:
-        print(site)
+        lambda_client.invoke(
+            FunctionName='scrape_and_store',
+            InvocationType='Event'
+            Payload = json.dumps({'site_name': site, 'site_rank': event['sites'].index(site)}) # if this doesn't work, try io.StringIO
+        )
