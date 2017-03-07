c111fa1e6185727fec6c58869399c8de41af0582
bdauer <dauer.benjamin@gmail.com>
2017-03-07 09:18:30 -0500

    Format lists for dynamodb, correct syntax

--- awssiteretriever.py
+++ awssiteretriever.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import requests
-from retrievermethods import get_data_from
+import boto3
+from retrievermethods import get_data_from, get_page, build_list_for_db
 from tablebuilder import add_item_to_table
 
 def lambda_handler(event, context):
@@ -9,22 +10,20 @@ def lambda_handler(event, context):
     try:
         page = get_page(site)
         headers, cookies, word_count = get_data_from(page)
-        site_data = ('site', 'S', site)
-        rank_data = ('rank', 'N', str(rank))
 
-        headers_for_db = []
-        for header in headers:
-            headers_for_db.append({'S': header})
-        headers_data = ('headers', 'L', headers_for_db)
-
-        cookies_data = ('cookies', 'L', cookies)
+        site_data = ('name', 'S', site)
+        rank_data = ('rank', 'N', str(rank + 1))
+        headers_data = build_list_for_db('headers', headers, 'S')
+        cookies_data = build_list_for_db('cookies', cookies, 'S')
         word_count_data = ('word_count', 'N', str(word_count))
-        attributes = [page_data, headers_data, cookies_data,
-                 word_count_data, site_data, rank_data]
+
+        attributes = [site_data, rank_data, headers_data,
+                      cookies_data, word_count_data]
 
         client = boto3.client('dynamodb', region_name='us-east-1')
         table_name = "siteDict"
-        add_item_to_table(client, table_name, attributes)
+        item_name = site
+        add_item_to_table(client, table_name, item_name, attributes)
 
     except requests.exceptions.ConnectionError:
         print("{0} could not be accessed".format(site))
